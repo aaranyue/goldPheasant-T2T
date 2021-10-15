54401b87f2b480cf22893ed1105c791455d64a8f
Luohao Xu <luohaox@gmail.com>
2021-10-15 11:09:56 +0200

    Update minimap-asm.sh

--- assembly/minimap-asm.sh
+++ assembly/minimap-asm.sh
@@ -3,4 +3,4 @@ ref=$1
 asm=$2
 cpu=12
 
-minimap2 --secondary=no -t $cpu -x asm5 $ref $asm | awk '$11>20000 && $10/$11>0.4' | sort -k6,6 -k8,8n > $ref.$asm.pdf
+minimap2 --secondary=no -t $cpu -x asm5 $ref $asm | awk '$11>20000 && $10/$11>0.4' | sort -k6,6 -k8,8n > $ref.$asm.paf
